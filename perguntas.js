ca3700358699c34baba2ca7faefb02877e0aa545
ig <igmiginc@gmail.com>
2024-12-02 15:15:08 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filosofia',
+    'Qual filósofo disse: "Se olhas por longo tempo para dentro de um abismo, o abismo também olha para dentro de ti." ?',
+    'Friedrich Nietzsche.'
+)
+
+criaCartao(
+    'Geopolítica',
+    'O que foi a Guerra ao Terror ?',
+    'Resposta dos EUA pós atentado do 11 de setembro a possíveis zonas terroristas no mundo.'
+)
+
+criaCartao(
+    'História',
+    'Qual era o motivo pelo conflito entre os portugueses e os espanhóis ?',
+    'foi uma guerra toritorial pela banda oriental, atual Uruguai.'
+)
+
+criaCartao(
+    'Relações Internacionais',
+    'qual a função do Estado ?',
+    'Regula a convivência dos habitantes de um país.'
+)
